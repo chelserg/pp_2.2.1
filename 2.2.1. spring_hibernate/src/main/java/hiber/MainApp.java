6a8e5fdee933399ff37b1435f3d355a9892339db
Sergey Chelyshev <sergey36russia@gmail.com>
2024-11-14 19:21:35 +0300

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,21 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car = new Car("bmw", 2011);
+      Car car2 = new Car("mercedes", 2014);
+      Car car3 = new Car("Vaz", 2003);
+      Car car4 = new Car("Uaz", 2015);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User6", "Lastname6", "user2@mail.ru", car2));
+      userService.add(new User("User7", "Lastname7", "user3@mail.ru", car3));
+      userService.add(new User("User8", "Lastname8", "user4@mail.ru", car4));
+      userService.add(new User("User5", "Lastname5", "user1@mail.ru", car));
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,7 +37,24 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
+
+
+      }
+      String carModel = "bmw";
+      int carSeries = 2011;
+
+      try {
+         User user = userService.getUserByCar(carModel, carSeries);
+         if (user != null) {
+            System.out.println("Найден пользователь:");
+            System.out.println(user);
+         } else {
+            System.out.println("Пользователь не найден.");
+         }
+      } catch (Exception e) {
+         System.out.println("Ошибка при поиске пользователя по машине: " + e.getMessage());
       }
 
       context.close();
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,12 +19,34 @@ public class User {
    @Column(name = "email")
    private String email;
 
-   public User() {}
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id", referencedColumnName = "id")
+private Car car;
    
-   public User(String firstName, String lastName, String email) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
+
+   }
+//   public User(String firstName, String lastName, String email) {
+//      this.firstName = firstName;
+//      this.lastName = lastName;
+//      this.email = email;
+//
+//   }
+
+
+   public User() {
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
    }
 
    public Long getId() {
@@ -57,5 +79,16 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
